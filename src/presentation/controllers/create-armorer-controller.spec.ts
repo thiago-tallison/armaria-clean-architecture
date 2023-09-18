4748eed1afeff7ffe1746480d42848fef02a7bd1
Thiago Tallison <thiagotallison@gmail.com>
2023-09-18 15:14:52 -0300

    test: ensure CreateArmorerController return 400 if passwords does not match

--- src/presentation/controllers/create-armorer-controller.spec.ts
+++ src/presentation/controllers/create-armorer-controller.spec.ts
@@ -60,6 +60,46 @@ describe('CreateArmorerController', () => {
 
   })
 
+  it('should return 400 if no passwordConfirmation is provided', () => {
+    const sut = new CreateArmorerController()
+
+    const httpRequest = {
+      body: {
+        registration: 'any-registration',
+        name: 'any-name',
+        email: 'any-email@mail.com',
+        password: 'any-password',
+        // passwordConfirmation: 'any-password',
+        phone: 'any-phone',
+      }
+    }
+
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new MissingParamError('passwordConfirmation'))
+
+  })
+
+  it('should return 400 if passwords does not match', () => {
+    const sut = new CreateArmorerController()
+
+    const httpRequest = {
+      body: {
+        registration: 'any-registration',
+        name: 'any-name',
+        email: 'any-email@mail.com',
+        password: 'any-password',
+        passwordConfirmation: 'different-password',
+        phone: 'any-phone',
+      }
+    }
+
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('Password and password confirmation must be equal'))
+
+  })
+
   it('should return 400 if no registration is provided', () => {
     const sut = new CreateArmorerController()
 
