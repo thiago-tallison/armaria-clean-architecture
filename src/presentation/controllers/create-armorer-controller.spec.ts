7a3dbbf10fb308687c14fead96842db21bb6f4f5
Thiago Tallison <thiagotallison@gmail.com>
2023-09-18 14:55:15 -0300

    test: ensure CreateArmorerController returns 400 if no registration is provided

--- src/presentation/controllers/create-armorer-controller.spec.ts
+++ src/presentation/controllers/create-armorer-controller.spec.ts
@@ -58,4 +58,23 @@ describe('CreateArmorerController', () => {
     expect(httpResponse.body).toEqual(new Error('Missing param: password'))
 
   })
+
+  it('should return 400 if no registration is provided', () => {
+    const sut = new CreateArmorerController()
+
+    const httpRequest = {
+      body: {
+        // registration: 'any-registration',
+        name: 'any-name',
+        email: 'any-email@mail.com',
+        password: 'any-password',
+        phone: 'any-phone',
+      }
+    }
+
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('Missing param: registration'))
+
+  })
 })
