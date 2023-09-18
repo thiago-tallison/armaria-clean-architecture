d884609c52d283c95c9c64839aa4bf1f9726f722
Thiago Tallison <thiagotallison@gmail.com>
2023-09-18 14:52:00 -0300

    test: ensure CreateArmorerController returns 400 if no email is provided

--- src/presentation/controllers/create-armorer-controller.spec.ts
+++ src/presentation/controllers/create-armorer-controller.spec.ts
@@ -20,4 +20,23 @@ describe('CreateArmorerController', () => {
     expect(httpResponse.body).toEqual(new Error('Missing param: name'))
 
   })
+
+  it('should return 400 if no email is provided', () => {
+    const sut = new CreateArmorerController()
+
+    const httpRequest = {
+      body: {
+        registration: 'any-registration',
+        name: 'any-name',
+        // email: 'any-email@mail.com',
+        password: 'any-password',
+        phone: 'any-phone',
+      }
+    }
+
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('Missing param: email'))
+
+  })
 })
