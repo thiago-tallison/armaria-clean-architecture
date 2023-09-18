27e7fcd6f08cae710933817724c7a6d4089cafa4
Thiago Tallison <thiagotallison@gmail.com>
2023-09-18 14:30:43 -0300

    test: ensure CreateArmorerController returns 400 if no name is provided

---
+++ src/presentation/controllers/create-armorer-controller.spec.ts
@@ -0,0 +1,21 @@
+import { describe, it, expect } from 'vitest'
+import { CreateArmorerController } from './create-armorer-controller'
+
+describe('CreateArmorerController', () => {
+  it('should return 400 if no name is provided', () => {
+    const sut = new CreateArmorerController()
+
+    const httpRequest = {
+      body: {
+        registration: 'any-registration',
+        // name: 'any-name',
+        email: 'any-email@mail.com',
+        password: 'any-password',
+        phone: 'any-phone',
+      }
+    }
+
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+  })
+})
