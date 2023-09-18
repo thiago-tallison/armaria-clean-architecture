8c10051466d727db3f2f52f1ea1992edcd5e093d
Thiago Tallison <thiagotallison@gmail.com>
2023-09-18 16:09:45 -0300

    refactor: create a ok helper method

--- src/presentation/controllers/create-armorer-controller.ts
+++ src/presentation/controllers/create-armorer-controller.ts
@@ -1,6 +1,6 @@
 import { InvalidParamError } from '../errors/invalid-params-error'
 import { MissingParamError } from '../errors/missing-params-error'
-import { badRequest } from '../helpers/http-helper'
+import { badRequest, ok } from '../helpers/http-helper'
 import { Controller } from '../protocols/controller'
 import { EmailValidator } from '../protocols/email-validator'
 import { HttpRequest, HttpResponse } from '../protocols/http'
@@ -21,7 +21,6 @@ export class CreateArmorerController implements Controller {
         return badRequest(new MissingParamError(field))
       }
     }
-
     if (httpRequest.body.password !== httpRequest.body.passwordConfirmation) {
       return badRequest(
         new Error('Password and password confirmation must be equal')
@@ -30,10 +29,6 @@ export class CreateArmorerController implements Controller {
     if(!this.emailValidator.isValid(httpRequest.body.email)){
       return badRequest(new InvalidParamError('email'))
     }
-
-    return {
-      statusCode: 200,
-      body: {},
-    }
+    return ok()
   }
 }
