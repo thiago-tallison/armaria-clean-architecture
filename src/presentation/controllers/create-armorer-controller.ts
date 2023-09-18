6296d539d3e451a92dd44e69acd33c5e26c8ab1a
Thiago Tallison <thiagotallison@gmail.com>
2023-09-18 15:14:32 -0300

    feat: ensure CreateArmorerController return 400 if passwords does not match

--- src/presentation/controllers/create-armorer-controller.ts
+++ src/presentation/controllers/create-armorer-controller.ts
@@ -4,11 +4,15 @@ import { HttpRequest, HttpResponse } from '../protocols/http'
 
 export class CreateArmorerController {
   handle(httpRequest: HttpRequest): HttpResponse {
-    const requiredFields = ['name', 'email', 'password', 'registration']
+    const requiredFields = ['name', 'email', 'password', 'registration', 'passwordConfirmation']
     for(const field of requiredFields) {
       if(!httpRequest.body[field]) {
         return badRequest(new MissingParamError(field))
       }
+    }
+
+    if(httpRequest.body.password !== httpRequest.body.passwordConfirmation) {
+      return badRequest(new Error('Password and password confirmation must be equal'))
     }
   }
 }
