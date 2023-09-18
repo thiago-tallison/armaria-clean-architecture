d129f976e847828c08475e3ba916c7d7c1da093f
Thiago Tallison <thiagotallison@gmail.com>
2023-09-18 15:18:09 -0300

    refactor: make CreateArmorerController implements Controller protocol

--- src/presentation/controllers/create-armorer-controller.ts
+++ src/presentation/controllers/create-armorer-controller.ts
@@ -1,8 +1,9 @@
 import { MissingParamError } from '../errors/missing-params-error'
 import { badRequest } from '../helpers/http-helper'
+import { Controller } from '../protocols/controller'
 import { HttpRequest, HttpResponse } from '../protocols/http'
 
-export class CreateArmorerController {
+export class CreateArmorerController implements Controller{
   handle(httpRequest: HttpRequest): HttpResponse {
     const requiredFields = ['name', 'email', 'password', 'registration', 'passwordConfirmation']
     for(const field of requiredFields) {
