d289650b139eae764b56ccf248d17e80329af6be
Thiago Tallison <thiagotallison@gmail.com>
2023-09-18 21:59:48 -0300

    test: ensure CreateArmorerController returns 500 if CreateArmorerUseCase throws

--- src/presentation/controllers/armorer/create-armorer-controller.spec.ts
+++ src/presentation/controllers/armorer/create-armorer-controller.spec.ts
@@ -5,8 +5,11 @@ import {
   EmailValidator,
   CreateArmorerUseCase,
 } from './create-armorer-protocols'
-import { InvalidParamError, MissingParamError, ServerError } from '../../errors'
-
+import {
+  InvalidParamError,
+  MissingParamError,
+  ServerError,
+} from '../../errors'
 
 const makeHttpRequest = (): HttpRequest => ({
   body: {
@@ -60,75 +63,75 @@ const makeSut = (): SutTypes => {
 }
 
 describe('CreateArmorerController', () => {
-  it('should return 400 if no name is provided', () => {
+  it('should return 400 if no name is provided', async () => {
     const { sut } = makeSut()
     const httpRequest = makeHttpRequest()
     delete httpRequest.body.name
-    const httpResponse = sut.handle(httpRequest)
+    const httpResponse = await sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new MissingParamError('name'))
   })
 
-  it('should return 400 if no email is provided', () => {
+  it('should return 400 if no email is provided', async () => {
     const { sut } = makeSut()
     const httpRequest = makeHttpRequest()
     delete httpRequest.body.email
-    const httpResponse = sut.handle(httpRequest)
+    const httpResponse = await sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new MissingParamError('email'))
   })
 
-  it('should return 400 if no password is provided', () => {
+  it('should return 400 if no password is provided', async () => {
     const { sut } = makeSut()
     const httpRequest = makeHttpRequest()
     delete httpRequest.body.password
-    const httpResponse = sut.handle(httpRequest)
+    const httpResponse = await sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new MissingParamError('password'))
   })
 
-  it('should return 400 if no passwordConfirmation is provided', () => {
+  it('should return 400 if no passwordConfirmation is provided', async () => {
     const { sut } = makeSut()
     const httpRequest = makeHttpRequest()
     delete httpRequest.body.passwordConfirmation
-    const httpResponse = sut.handle(httpRequest)
+    const httpResponse = await sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(
       new MissingParamError('passwordConfirmation')
     )
   })
 
-  it('should return 400 if no registration is provided', () => {
+  it('should return 400 if no registration is provided', async () => {
     const { sut } = makeSut()
     const httpRequest = makeHttpRequest()
     delete httpRequest.body.registration
-    const httpResponse = sut.handle(httpRequest)
+    const httpResponse = await sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new MissingParamError('registration'))
   })
 
-  it('should return 400 if passwords does not match', () => {
+  it('should return 400 if passwords does not match', async () => {
     const { sut } = makeSut()
     const httpRequest = makeHttpRequest()
     httpRequest.body.passwordConfirmation = 'invalid-password'
-    const httpResponse = sut.handle(httpRequest)
+    const httpResponse = await sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(
       new InvalidParamError('passwordConfirmation')
     )
   })
 
-  it('should return 400 if email is not valid', () => {
+  it('should return 400 if email is not valid', async () => {
     const { sut, emailValidatorStub } = makeSut()
     const httpRequest = makeHttpRequest()
     httpRequest.body.email = 'invalid-email'
     vi.spyOn(emailValidatorStub, 'isValid').mockReturnValueOnce(false)
-    const httpResponse = sut.handle(httpRequest)
+    const httpResponse = await sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new InvalidParamError('email'))
   })
 
-  it('should call EmailValidator with correct value', () => {
+  it('should call EmailValidator with correct value', async () => {
     const { sut, emailValidatorStub } = makeSut()
     const isValid = vi.spyOn(emailValidatorStub, 'isValid')
     const httpRequest = makeHttpRequest()
@@ -137,25 +140,25 @@ describe('CreateArmorerController', () => {
     expect(isValid).toHaveBeenCalledWith(httpRequest.body.email)
   })
 
-  it('should return 500 if EmailValidator throws', () => {
+  it('should return 500 if EmailValidator throws', async () => {
     const { sut, emailValidatorStub } = makeSut()
     vi.spyOn(emailValidatorStub, 'isValid').mockImplementationOnce(() => {
       throw new Error()
     })
     const httpRequest = makeHttpRequest()
-    const httpResponse = sut.handle(httpRequest)
+    const httpResponse = await sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(500)
     expect(httpResponse.body).toEqual(new ServerError())
   })
 
-  it('should return 200 if all data is valid', () => {
+  it('should return 200 if all data is valid', async () => {
     const { sut } = makeSut()
     const httpRequest = makeHttpRequest()
-    const httpResponse = sut.handle(httpRequest)
+    const httpResponse = await sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(201)
   })
 
-  it('should call CreateArmorerUseCase with correct values', () => {
+  it('should call CreateArmorerUseCase with correct values', async () => {
     const { sut, createArmorerUseCaseStub } = makeSut()
     const execute = vi.spyOn(createArmorerUseCaseStub, 'execute')
     const httpRequest = makeHttpRequest()
@@ -169,5 +172,16 @@ describe('CreateArmorerController', () => {
       registration,
       phone,
     })
+  })
+
+  it('should throw if CreateArmorerUseCase throws', async () => {
+    const { sut, createArmorerUseCaseStub } = makeSut()
+    vi.spyOn(createArmorerUseCaseStub, 'execute').mockImplementationOnce(() => {
+      throw new Error()
+    })
+    const httpRequest = makeHttpRequest()
+    const httpResponse =  await sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(500)
+    expect(httpResponse.body).toEqual(new ServerError())
   })
 })
