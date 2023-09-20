2501fadf3881b9550debf085493756c1e7b3d9d6
Thiago Tallison <thiagotallison@gmail.com>
2023-09-20 11:19:02 -0300

    refactor: ensure EmailValidatorAdapter.isValid is called with correct email

--- src/utils/email-validator-adapter.spec.ts
+++ src/utils/email-validator-adapter.spec.ts
@@ -20,4 +20,13 @@ describe('EmailValidator Adapter', () => {
     const isValid = sut.isValid('valid_email@mail.com')
     expect(isValid).toBe(true)
   })
+
+  it('ensure EmailValidatorAdapter.isValid is called with correct email', () => {
+    const parseSpy = vi.spyOn(emailSchema, 'parse')
+    const sut = new EmailValidatorAdapter()
+    const email = 'any_email@mail.com'
+    sut.isValid(email)
+    expect(parseSpy).toHaveBeenCalledWith(email)
+    expect(parseSpy).toHaveBeenCalledTimes(1)
+  })
 })
