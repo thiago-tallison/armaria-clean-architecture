3063ff0525f30b974a16b79401828944d6a3789f
Thiago Tallison <thiagotallison@gmail.com>
2023-09-20 11:41:32 -0300

    refactor: move sut creation to a helper method

---
+++ src/data/usecases/armorer/db-create-armorer-use-case.spec.ts
@@ -0,0 +1,40 @@
+import { Encryptor } from '@/data/protocols/encryptor'
+import { describe, expect, it, vi } from 'vitest'
+import { DBCreteArmorerUseCase } from './db-create-armorer-use-case'
+
+type SutType = {
+  sut: DBCreteArmorerUseCase
+  encryptorStub: Encryptor
+}
+
+const makeArmorerData = () => ({
+  registration: 'any_registration',
+  name: 'any_name',
+  email: 'any_email@mail.com',
+  password: 'any_password',
+  phone: 'any_phone_number',
+})
+
+const makeSut = (): SutType => {
+  class EncryptorStub implements Encryptor {
+    async encrypt(value: string): Promise<string> {
+      return new Promise(resolve => resolve(`${value}_encrypted`))
+    }
+  }
+
+  const encryptorStub = new EncryptorStub()
+
+  const sut = new DBCreteArmorerUseCase(encryptorStub)
+
+  return { sut, encryptorStub }
+}
+
+describe('DBCreteArmorer UseCase', () => {
+  it('should call Encryptor with correct password', () => {
+    const armorerData = makeArmorerData()
+    const { encryptorStub, sut } = makeSut()
+    const encryptorSpy = vi.spyOn(encryptorStub, 'encrypt')
+    sut.create(armorerData)
+    expect(encryptorSpy).toHaveBeenCalledWith(armorerData.password)
+  })
+})
