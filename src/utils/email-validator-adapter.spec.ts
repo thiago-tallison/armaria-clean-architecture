34e8a9fa415ab7b1f87be0baa6553b1677abb94b
Thiago Tallison <thiagotallison@gmail.com>
2023-09-20 00:57:20 -0300

    refactor: make EmailValidatorAdapter implements zod

--- src/utils/email-validator-adapter.spec.ts
+++ src/utils/email-validator-adapter.spec.ts
@@ -7,4 +7,10 @@ describe('EmailValidator Adapter', () => {
     const isValid = sut.isValid('invalid_email')
     expect(isValid).toBe(false)
   })
+
+  it('should return true if validator returns true', () => {
+    const sut = new EmailValidatorAdapter()
+    const isValid = sut.isValid('valid_email@mail.com')
+    expect(isValid).toBe(true)
+  })
 })
--- src/utils/email-validator-adapter.ts
+++ src/utils/email-validator-adapter.ts
@@ -1,8 +1,14 @@
 import { EmailValidator } from '@/presentation/protocols/email-validator'
+import { z } from 'zod'
 
 export class EmailValidatorAdapter implements EmailValidator {
   isValid(email: string): boolean {
-    console.log(email)
-    return false
+    const emailSchema = z.string().email()
+    try {
+      emailSchema.parse(email)
+      return true
+    } catch (err) {
+      return false
+    }
   }
 }
