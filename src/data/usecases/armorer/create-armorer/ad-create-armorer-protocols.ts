6b79faa4506621e72ec22bb18a1683550d70e523
Thiago Tallison <thiagotallison@gmail.com>
2023-09-20 13:31:30 -0300

    refactor: move CreateArmorerRepository to protocols folder

--- src/data/usecases/armorer/create-armorer/ad-create-armorer-protocols.ts
+++ src/data/usecases/armorer/create-armorer/ad-create-armorer-protocols.ts
@@ -1,3 +1,3 @@
+export * from '@/data/protocols/create-armorer-repository'
 export * from '@/data/protocols/encryptor'
-export * from '@/data/usecases/armorer/create-armorer/create-armorer-repository'
 export * from '@/domain/usecases/armorer/create-armorer-usecase'
