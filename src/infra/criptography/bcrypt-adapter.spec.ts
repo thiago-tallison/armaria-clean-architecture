09da140270837c50481c7e4f3b16ce129335d6e5
Thiago Tallison <thiagotallison@gmail.com>
2023-09-21 12:42:36 -0300

    refactor: move sut creation to a helper method

--- src/infra/criptography/bcrypt-adapter.spec.ts
+++ src/infra/criptography/bcrypt-adapter.spec.ts
@@ -8,29 +8,41 @@ jest.mock('bcrypt', () => ({
   }
 }))
 
+type SutTypes = {
+  salt: number
+  sut: BcrypterAdapter
+}
+
+const makeSut = (): SutTypes => {
+  const salt = 12
+  const sut = new BcrypterAdapter(salt)
+
+  return {
+    salt,
+    sut
+  }
+}
+
 describe('BcrypterAdapter', () => {
   afterEach(() => {
     jest.clearAllMocks()
   })
 
   it('should call bcrypt with correct values', async () => {
-    const salt = 12
-    const sut = new BcrypterAdapter(salt)
+    const { sut, salt } = makeSut()
     const hashSpy = jest.spyOn(bcrypt, 'hash')
     await sut.encrypt('any_value')
     expect(hashSpy).toHaveBeenCalledWith('any_value', salt)
   })
 
   it('should return a hash on success', async () => {
-    const salt = 12
-    const sut = new BcrypterAdapter(salt)
+    const { sut } = makeSut()
     const hashValue = await sut.encrypt('any_value')
     expect(hashValue).toEqual('hash')
   })
 
   it('should throw if bcrypt throws', async () => {
-    const salt = 12
-    const sut = new BcrypterAdapter(salt)
+    const { sut } = makeSut()
     jest.spyOn(bcrypt, 'hash').mockImplementationOnce(async () => {
       return new Promise((resolve, reject) => reject(new Error()))
     })
