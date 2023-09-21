a2dd41cc42f30869ede06a5e7313251dc5de08df
Thiago Tallison <thiagotallison@gmail.com>
2023-09-21 14:00:29 -0300

    chore: configure fastify

---
+++ src/main/server.ts
@@ -0,0 +1,17 @@
+import Fastify from 'fastify'
+
+const fastify = Fastify({
+  logger: true
+})
+
+fastify.get('/', function (request, reply) {
+  reply.send({ hello: 'world' })
+})
+
+fastify.listen({ port: 3000 }, function (err, _address) {
+  if (err) {
+    fastify.log.error(err)
+    process.exit(1)
+  }
+  console.log(`server listening on ${_address}`)
+})
